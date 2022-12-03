94e2ef9bc53ef17fc3dab452d9db2bddcb28535a
MikeChigev <mchigev@gmail.com>
2022-12-03 23:42:15 +0300

    число A в натуральную степень B.

---
+++ Work/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16Console.Clear();
+
+
+int DataEntry(int DigitA, int DigitB)
+{
+    int result = 1;
+    for (int i = 1; i <= DigitB; i++)
+    {
+        result = result * DigitA;
+    }
+    return result;
+}
+Console.WriteLine("Введите число A: ");
+int DigitA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int DigitB = int.Parse(Console.ReadLine());
+
+
+int result = DataEntry(DigitA, DigitB);
+Console.WriteLine(" Число " + DigitA + " в степени " + DigitB + " = " + result);
+
+
+
