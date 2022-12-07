572bcc9f3d5ce00e2b9692078ef1b73e88f85f7d
MikeChigev <mchigev@gmail.com>
2022-12-07 13:25:15 +0300

    Task3 задаёт массив из 8 элементов и выводит их на экран.

---
+++ Work/Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+
+int[] array = new int[8];
+Random rand = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(1, 10);
+
+}
+
+Console.WriteLine("[" + string.Join(", ", array) + "]");
